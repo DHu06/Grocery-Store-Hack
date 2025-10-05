16900e719d6d23b06f46072ad2c7e639af14de42
justin75p <70132668+justin75p@users.noreply.github.com>
2025-10-04 20:02:20 -0700

    helper method using haversine formula to compute shortest distance between two points

--- server.js
+++ server.js
@@ -79,6 +79,24 @@ app.post('api/sort-by-distance', (req, res) => {
 
     }
 });
+
+// Haversine formula to find shortest distance between two points
+function sortByDistance(lat1, lng1, lat2, lng2) {
+
+    // difference in latitude
+    const dLat = (lat2 - lat1) * Math.PI / 180;
+    // difference in longitude
+    const dLng = (lng2 - lng1) * Math.PI / 180;
+
+    // radius of earth
+    const r = 6371;
+
+    const a = Math.pow(Math.sin(dLat/2), 2) + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * Math.pow(Math.sin(dLng/2), 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    return r * c;
+
+} 
   
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
