c1c45f7fca1df6173cddc626b451bf0814a11086
justin75p <70132668+justin75p@users.noreply.github.com>
2025-10-04 19:25:09 -0700

    implementation of sort by price endpoint and declaration of sort by distance endpoint

--- server.js
+++ server.js
@@ -48,6 +48,37 @@ app.post('/api/identify', async(req, res) => {
 
     }
 });
+
+app.post('api/sort-by-price', (req, res) => {
+    try {
+        const {stores} = req.body;
+        if (!stores) {
+            return res.status(400).json({
+                success: false,
+                error: 'Invalid stores data'
+            })
+        }
+        const sorted = stores.sort((a,b) => a.price - b.price);
+
+        res.json({
+            success: true,
+            stores: sorted
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            error: error.message
+        });
+    }
+});
+
+app.post('api/sort-by-distance', (req, res) => {
+    try {
+
+    } catch {
+
+    }
+});
   
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
